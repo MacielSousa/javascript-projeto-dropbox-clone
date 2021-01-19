cb3149d7da7887e5e8c4dd1d2da3497eb46d0610
Maciel Sousa <fmacielsousa@gmail.com>
2021-01-19 19:54:03 -0300

    Recebendo arquivos no Nodejs com Formidable

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var formdable = require('formidable');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/upload', (req, res) => {
+
+  let form = new formdable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions: true
+  });
+
+  form.parse(req, (err, fields, files) => {
+
+    res.json({
+      files: files
+    });
+
+  });
+  
 });
 
 module.exports = router;
